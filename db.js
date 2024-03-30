b43d4276c847998bbe2e987a3f50ae0a3ee87d72
abdullahz34 <usamash34@gmail.com>
2024-03-30 04:48:37 +0000

    fix: typo env var

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
