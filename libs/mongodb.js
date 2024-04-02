c735abb6da93f0dd87aa5527f6b5895c93a93b96
OnCnT <OnCnTao@gmail.com>
2024-04-02 21:19:39 +0100

    changed database to main database

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectMongoDB = async () =>{
     try{
-        await mongoose.connect("mongodb+srv://oncntao:swepmongodb@moodlift.dbujelh.mongodb.net/moodlift")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB");
     }
     catch (error){
