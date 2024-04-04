4f63857f074dde3140a376b2f6de7ea81e2d9aac
Ismail <ismailhussain2323@gmail.com>
2024-04-04 12:20:06 +0100

    WORKING survey implemented with main -functioning create survey -functioning login

---
+++ libs/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectMongoDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MongoDB connected");
+    }
+    catch (error){
+        console.log(error);
+    }
+};
+
+export default connectMongoDB;
