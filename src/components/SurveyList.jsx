a19ba78ab4b74c8edbdaa3a7a9966fa2abfddad6
Ismail Hussain <ismailhussain2323@gmail.com>
2024-04-05 18:32:44 +0100

    added frequency viewing on dashboard

--- src/components/SurveyList.jsx
+++ src/components/SurveyList.jsx
@@ -5,6 +5,10 @@ import { HiPencilAlt, HiDotsHorizontal } from "react-icons/hi";
 import React from 'react';
 import { IoMenu } from "react-icons/io5";
 
+const capitalFirst = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 const getSurveys = async () => {
   try {
     const res = await fetch("http://localhost:3000/api/surveys", {
@@ -28,12 +32,16 @@ export default async function SurveyList() {
     <>
     <div className="pt-3 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {surveys.map((s) => (
+        
         <div
           key={s._id}
           className="card shadow-xl bg-primary border-secondary border-3"
         >
           <div className="card-body">
             <h2 className="card-title">{s.title}</h2>
+            <h3 className="whitespace-nowrap overflow-hidden max-w-max text-m font-primary bg-secondary text-white px-2 py-1 rounded-md inline-block">
+            {capitalFirst(s.frequency)}
+</h3>
             <div className="card-actions justify-end">{s.description}</div>
           </div>
 
