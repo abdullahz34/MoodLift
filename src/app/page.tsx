5188b30bb644c74651cafb4f0308d0cd9f79847e
Zayan-Syed <zayan_s@outlook.com>
2024-04-07 17:10:12 +0100

    resolve conflicts attempt 3

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+
+  if (session) return redirect("/dashboard")
+
+  return redirect("/login")
+}
