638f000bec65d249c785ecec6bee97632bf1f594
abdullahz34 <usamash34@gmail.com>
2024-04-09 06:07:46 +0100

    date, edit, delete feat setup

--- models/todoSchema.js
+++ models/todoSchema.js
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const taskSchema = new Schema({
+  text: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+});
+
 const todoSchema = new Schema({
   username: { type: String, required: true },
   date: { type: Date, required: true },
-  tasks: [{ type: String, required: true }],
+  tasks: [taskSchema],
 });
 
 export default mongoose.models.Todo || mongoose.model('Todo', todoSchema);
--- src/app/api/todolists/route.js
+++ src/app/api/todolists/route.js
@@ -8,7 +8,6 @@ export const GET = async (request) => {
     const { searchParams } = new URL(request.url);
     const username = searchParams.get('username');
     const date = searchParams.get('date');
-
     const todos = await Todo.find({ username, date });
     return NextResponse.json(todos, { status: 200 });
   } catch (error) {
@@ -22,14 +21,45 @@ export const POST = async (request) => {
   try {
     const { username, date, tasks } = await request.json();
     await connect();
-
     const newTodoList = new Todo({ username, date, tasks });
     await newTodoList.save();
-
     return NextResponse.json({ message: 'To-do list created successfully' }, { status: 201 });
   } catch (error) {
     console.error('Error in creating to-do list:', error);
     const errorMessage = error.message || 'An error occurred while creating to-do list';
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
+};
+
+export const PUT = async (request) => {
+  try {
+    const { username, date, tasks } = await request.json();
+    await connect();
+    const todoList = await Todo.findOneAndUpdate(
+      { username, date },
+      { tasks },
+      { new: true, upsert: true }
+    );
+    return NextResponse.json(todoList, { status: 200 });
+  } catch (error) {
+    console.error('Error in updating to-do list:', error);
+    const errorMessage = error.message || 'An error occurred while updating to-do list';
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
+};
+
+export const DELETE = async (request) => {
+  try {
+    const { username, date } = await request.json();
+    await connect();
+    const deletedTodoList = await Todo.findOneAndDelete({ username, date });
+    if (!deletedTodoList) {
+      return NextResponse.json({ message: 'To-do list not found' }, { status: 404 });
+    }
+    return NextResponse.json({ message: 'To-do list deleted successfully' }, { status: 200 });
+  } catch (error) {
+    console.error('Error in deleting to-do list:', error);
+    const errorMessage = error.message || 'An error occurred while deleting to-do list';
     return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 };
