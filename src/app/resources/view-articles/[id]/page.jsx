8eea4594e84a545b2ae170e43a727b16527df689
OnCnT <OnCnTao@gmail.com>
2024-04-09 16:19:33 +0100

    added authentication for articles

--- src/app/resources/view-articles/[id]/page.jsx
+++ src/app/resources/view-articles/[id]/page.jsx
@@ -1,6 +1,8 @@
 import styles from "./singleArticle.module.css";
 import Link from "next/link";
 import DeleteButton from "@/components/DeleteButton";
+import { getServerSession } from "next-auth";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
 const getData = async (id) => {
     const res = await fetch(`http://localhost:3000/api/articles/${id}`, {
@@ -18,6 +20,8 @@ const singlePage = async({params}) => {
     const {id} = params;
     const {article} = await getData(id);
 
+    const session = await getServerSession(authOptions);
+    if (session && (session.user.type === 'Admin' || session.user.type === 'Ambassador')) {
     return(
       <div className="flex justify-center h-screen">
         <div className="w-1/2">
@@ -39,6 +43,26 @@ const singlePage = async({params}) => {
         </div>
       </div>
     )
+  }
+  else{
+    return(
+      <div className="flex justify-center h-screen">
+        <div className="w-1/2">
+          <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              <img src={article.imgURL} className="max-w-sm rounded-lg shadow-2xl" />
+              <div>
+                <h1 className="text-5xl font-bold">{article.title}</h1>
+                <p className="py-6">{article.summary}</p>
+              </div>
+            </div>
+          </div>
+
+          <div dangerouslySetInnerHTML={{ __html: article.content }} className={`${styles.articleContent} article-content`}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default singlePage;
