6ae8182df780d58de046c1799a1caf37afe87721
abdullahz34 <usamash34@gmail.com>
2024-04-09 05:46:20 +0100

    feats:  mood logger + streak gamification

--- models/journalSchema.js
+++ models/journalSchema.js
@@ -6,6 +6,8 @@ const journalSchema = new Schema({
   username: { type: String, required: true },
   date: { type: Date, required: true },
   entry: { type: String, required: true },
+  mood: { type: String, default: '' }, 
+  streak: { type: Number, default: 0 },
 });
 
 export default mongoose.models.Journal || mongoose.model('Journal', journalSchema);
--- src/app/api/journals/route.js
+++ src/app/api/journals/route.js
@@ -8,7 +8,6 @@ export const GET = async (request) => {
     const { searchParams } = new URL(request.url);
     const username = searchParams.get('username');
     const date = searchParams.get('date');
-
     const journals = await Journal.find({ username, date });
     return NextResponse.json(journals, { status: 200 });
   } catch (error) {
@@ -20,12 +19,27 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
-    const { username, date, entry } = await request.json();
+    const { username, date, entry, mood } = await request.json();
     await connect();
 
-    const newJournalEntry = new Journal({ username, date, entry });
-    await newJournalEntry.save();
+    // Update the user's streak
+    const lastEntry = await Journal.findOne({ username }).sort({ date: -1 });
+    let streak = 0; // Set the initial streak to 0 for a new entry
+    if (lastEntry) {
+      const lastEntryDate = new Date(lastEntry.date);
+      const today = new Date(date);
+      const timeDiff = today.getTime() - lastEntryDate.getTime();
+      const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
+      if (diffDays === 1) {
+        streak = lastEntry.streak + 1; // Increment the streak if the new entry is on the next day
+      } else {
+        streak = 0; // Reset the streak if there's a gap between entries
+      }
+    }
+
+    const newJournalEntry = new Journal({ username, date, entry, mood, streak });
+    await newJournalEntry.save();
     return NextResponse.json({ message: 'Journal entry created successfully' }, { status: 201 });
   } catch (error) {
     console.error('Error in creating journal entry:', error);
