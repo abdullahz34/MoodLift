b34f391f0f16111384ffb2dc47b0bb94b04c23ab
Ismail Hussain <ismailhussain2323@gmail.com>
2024-04-10 11:03:35 +0100

    working algorithm displaying red/green

--- src/app/infographics/SurveyListItem.js
+++ src/app/infographics/SurveyListItem.js
@@ -14,11 +14,26 @@ const SurveyListItem = ({ survey }) => {
       [username]: !prevState[username],
     }));
   };
+  
+  const getUsernameColour = (answers) => {
+    const numericValues = answers.filter((answer) => !isNaN(Number(answer))).map(Number);
+    const totalValue = numericValues.reduce((sum, value) => sum + value, 0);
+    const colourIndex = totalValue % 8;
+    return colourIndex < 4 ? 'bg-error' : 'bg-success';
+    //is the user happy or sad, using green or red in an algorithm to reflect this by totalling their 
+    //number scores and taking an average over the number of questions in the survey
+    //if the score is low = red
+    //else = green
+
+  };
+
 
   return (
+    <div className='px-3 pt-3'>
     <div className="border-2 bg-neutral-content border-gray-300 p-3 mb-4 rounded-md">
-      <div className="flex justify-between items-center mb-4">
+      <div className="flex justify-between items-center mb-4 pl-1">
         <h3 className="text-lg font-bold">{survey.title}</h3>
+        
         {/*possibly better styling*/ }
       </div>
             {/*map each survey questions to an index along with usernames answers from mongo*/ }
@@ -26,7 +41,9 @@ const SurveyListItem = ({ survey }) => {
         <div key={index} className="mb-4">
           <div className="flex justify-between items-center">
             <div className="flex items-center">
-              <p className="font-bold mr-4">{response.username}</p>
+            <p className={` font-bold mr-4 px-2 py-1 rounded-md ${getUsernameColour(response.answers)}`}>
+                {response.username}
+              </p>
               <button
                 className="btn btn-ghost text-m text-bg-secondary mr-3"
                 onClick={() => toggleUserResponses(response.username)}
@@ -53,6 +70,7 @@ const SurveyListItem = ({ survey }) => {
           )}
         </div>
       ))}
+    </div>
     </div>
   );
 };
