27eb6465a179d38a107e618b8e8641122a146a7f
Yaser E Gh E Dh Alharbi <y.alharbi@se22.qmul.ac.uk>
2024-04-10 08:58:51 +0100

    Fixed Calendar deselect issue

--- src/components/RequestAmbassador/index.tsx
+++ src/components/RequestAmbassador/index.tsx
@@ -7,7 +7,13 @@ const index: FC<indexProps> = ({}) => {
     return(
 
         <div className="flex flex-col backdrop-blur-sm bg-slate-500/5  rounded-3xl h-full w-96 p-10">
-            RequestAmbassador
+            <details className="dropdown">
+            <summary className="m-1 btn">Ambassadors</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+            </ul>
+            </details>
         </div>
     )
 }
